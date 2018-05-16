45e313fa0f8cd3a6364e89167b8306bb0e8b4fcc
Saroj <saroj.joshi@doconsultants.com>
2018-05-16 18:23:20 +0545

    Added ciphertext after generated from gcloud cmd

--- kms.tf
+++ kms.tf
@@ -7,3 +7,8 @@ resource "google_kms_crypto_key" "crypto_key" {
   name     = "kms-secret-key"
   key_ring = "${google_kms_key_ring.key_ring.id}"
 }
+
+data "google_kms_secret" "usersecret" {
+  crypto_key = "${google_kms_crypto_key.crypto_key.id}"
+  ciphertext = "CiQAvo+g7Tp7NzRemJcG352L7CcYJCefLMJT6cOI+nC7K8rKm0USMgD6sjTT8tv5tVrYyck+Hy4GKNcWHgZBZTSP6AlqZnCjzarbQ59eisEvvxaRyqN3HsP2"
+}
